758517ef6546883ad6d270a6ea797a4407bc2643
Raphael Lima Fernandes <raphael.lima05@gmail.com>
2018-05-27 17:54:34 -0300

    Composite Pattern added

---
+++ CompositePattern/CompositePattern/Folder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    public class Folder : AObject
+    {
+        private List<AObject> Objects = new List<AObject>();
+
+        public Folder(string name) : base(name)
+        {
+        }
+
+        public void AddObject(AObject @object)
+        {
+            @object.Level = Level + 1;
+            Objects.Add(@object);
+        }
+
+        public override void Display()
+        {
+            var alignment = GetAlignment();
+
+            Console.WriteLine(string.Concat(alignment, Name));
+
+            foreach (var @object in Objects)
+            {
+                @object.Display();
+            }
+        }
+     
+    }
+}
---
+++ CompositePattern/CompositePattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Folder("C:");
+
+            var desktop = new Folder("Desktop");
+            root.AddObject(desktop);
+            var historyHomework = new File("History Homework.docx");
+            var myVideo = new File("My Video.mp4");
+
+            var media = new Folder("Media");
+
+            var pictures = new Folder("Pictures");
+
+            var myPicture = new File("Picture.jpg");
+
+            desktop.AddObject(historyHomework);
+
+            desktop.AddObject(media);
+
+            media.AddObject(pictures);
+
+            pictures.AddObject(myPicture);
+
+            var videos = new Folder("Videos");
+            root.AddObject(videos);
+
+            videos.AddObject(myVideo);
+
+            var presentation = new File("Presentation.pptx");
+
+            desktop.AddObject(presentation);
+
+            root.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
